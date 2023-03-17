3f4c10884d3818c3fcecc99708f3794e2166afad
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-03-17 10:16:00 +0900

    230317-5-1015

--- 용사애니메이션/nodeAnimSample.js
+++
@@ -1,9 +0,0 @@
-
-export class NodeAnimSample{
-    constructor(loopType, frameDelay , animationName , imageNameList){
-        this.loopType = loopType;
-        this.frameDelay = frameDelay;
-        this.animationName = animationName;
-        this.imageNameList = imageNameList;
-    }
-}
